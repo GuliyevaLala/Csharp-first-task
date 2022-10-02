d8e1a6eeb5da0c00c2c50fb76197c1db04347503
Lala I. Guliyeva <lalaig@code.local>
2022-10-02 13:19:16 +0400

    Correct version of previous task

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,44 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter valid number: ");
+        Num: int num = Convert.ToInt32(Console.ReadLine());
+            int j = 0;
+
+            if (num == 1 || num == 0)
+            {
+                Console.WriteLine("Opps! Something went wrong! Try again!");
+                goto Num;
+
+            }
+            else
+            {
+                for (int i = 1; i <= num; i++)
+                {
+                    if (num % i == 0)
+                    {
+                        j++;
+                    }
+
+                }
+
+                if (j == 2)
+                {
+                    Console.WriteLine("{0} is a Prime Number", num);
+                    Console.WriteLine("Press any key to exit.");
+                    Console.ReadKey(true);
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("{0} is a composite", num);
+                    Console.WriteLine("Press any key to exit.");
+                    Console.ReadKey(true);
+                    return;
+
+                }
+            }
         }
+
     }
 }
